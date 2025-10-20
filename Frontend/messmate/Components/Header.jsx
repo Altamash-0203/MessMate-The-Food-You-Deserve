e9593ea68d6c3756758f8e60faaecbc57db14abc
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-10-20 19:52:00 +0530

    layed the frontend foundation of project and added the logo

---
+++ Frontend/messmate/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div>
+            <img src="../public/logo.svg" alt="" />
+            <h1>Header</h1>
+        </div>
+    );
+}
---
+++ Frontend/messmate/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Header from '../components/Header.jsx';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        < Header />
+       <h1>Welcome </h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
