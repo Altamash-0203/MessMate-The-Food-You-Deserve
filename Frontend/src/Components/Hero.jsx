8ee49a05dfc1e17cd7f81f73fe08d1ee104f173c
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-10-26 02:28:41 +0530

    minor things

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -1,26 +1,56 @@
 function Hero() {
   return (
-    <section className = "bg-gray-100 ">
-      <div className="flex justify-between">
-        <div className="">
-          <h1 className="font-bold text-4xl">Digitalize Your Daily Meal Experience</h1>
-          <p className="font-semibold text-sm">Choose from fresh, healthy options every day with ease.</p>
-          <div className="p-4">
-            <button className="bg-red-700 text-white rounded-full p-2 mr-4">Get Started</button>
-            <button className="bg-red-700 text-white rounded-full p-2">Watch Demo</button>
+    <div>
+      <section className="bg-gray-100 ">
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-center items-center text-center">
+            <h1 className="font-bold text-6xl">
+              Digitalize Your <span className="text-red-700">Daily Meal</span>{" "}
+              Experience
+            </h1>
+            <p className="font-semibold text-lg pt-4">
+              Healthy , tasty and ready in minutes
+            </p>
+            <div className="p-4">
+              <button className="bg-red-500 text-white font-bold rounded-full p-2 mr-4">
+                Explore menu
+              </button>
+            </div>
           </div>
-        </div>
-     
+
           <div className="">
             <img
               className="rounded-lg h-80 w-120"
               src="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?q=80&w=1200&auto=format&fit=crop"
               alt="Colorful healthy bowl"
             />
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+
+      <section className="flex justify-evenly mt-8">
+
+        <div className="border border-gray-100 bg-gray-100 shadow-xs hover:shadow-xl rounded-xl flex flex-col gap-4 p-12 pl-20 pr-20">
+          <h2 className="font-extrabold">Healthy Option</h2>
+          <p>
+            Nutritious meals <br /> tailored to your needs
+          </p>
+        </div>
+
+        <div className="border border-gray-100 bg-gray-100 shadow-xs hover:shadow-xl rounded-xl flex flex-col gap-4 p-12 pl-20 pr-20">
+          <h2 className="font-extrabold">Fast Delivery</h2>
+          <p>
+            30 minutes from <br /> order to the door{" "}
+          </p>
+        </div>
+
+        <div className="border border-gray-100 bg-gray-100 shadow-xs hover:shadow-xl rounded-xl flex flex-col gap-4 p-12 pl-20 pr-20">
+          <h2 className="font-extrabold">Personalized Menu</h2>
+          <p>Choose what you love/p</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
